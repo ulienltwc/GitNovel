e81c548ce07a7d49844b418e401e4d04609ba60d
ulienltwc <yk.bkcat.0411@gmail.com>
2022-11-08 23:49:02 +0900

    all

--- src/components/RogIn.jsx
+++ src/components/RogIn.jsx
@@ -41,51 +41,9 @@ const button = css`
 
 
 const RogIn = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
-  return (
+    return (
     <div className={formContainer}>
-      <form className={form}>
-        <h1 className={RogInTitle}>
-          GitNovelにログイン
-        </h1>
-        <div className={uiForm}>
-        <Box className={formField} component="formField" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="メールアドレス"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-          </Box> 
-          <Box className={formField} component="formField" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="パスワード"
-              name="password"
-              autoComplete="password"
-              autoFocus
-            />
-          </Box> 
-          <Button className={button} variant='contained' sx={{ mt: 2 }}>
-          ログイン
-          </Button>
-        </div>
-      </form>
+
     </div>
   )
 }
--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -1,41 +1,67 @@
 import React from 'react'
 import { css } from '@emotion/css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, cssVar } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
 
-
-
-
-const logo = css`
-  display: block;
-    margin-left: auto;
-    margin-right: auto
-`
-const searchBox = css`
-  background-color: #ffffff;
-  align-items: center;
+const box = css`
+  display: flexbox;
   justify-content: center;
-  margin-right: 20%;
-  margin-left: 20%;
+  margin-left: 200px;
+  margin-right: 200px;
+  margin-top: 10px;
+  background-color: #faebf1;
   border-radius: 20px;
   border: #555;
-  box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.3);
+`
+const largeBox = css`
+  background-color: #a2f7f7;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+const logo = css`
 `
 
-const searchIcon = css`
-  margin-top: 2%;
-  margin-bottom: 1%;
-  margin-left: 3%;
+const img = css`
+  margin-left: auto;
+  margin-right: auto;
+`
+const search = css`
+  margin-left: 200px;
+  margin-right: 200px;
+  margin-top: 10px;
+`
+const searchForm = css`
+  &:focus {
+    background-color: #ffceda;
+    box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.3);
+  }
+  
+  border-radius: 3px;
+  margin-top: 10px;
+  box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.3);
 `
 
 
 const TopPage = () => {
   return (
     
-    <div>
-      <img className={logo} src="https://3.bp.blogspot.com/-FJiaJ8gidCs/Ugsu-rSFw0I/AAAAAAAAXNA/JFiIUoxggW4/s400/book_tate.png" alt="GirNovel"></img>
-      <a></a>
-      <br />
-      <a href='./SignIn'>SignIn</a>
+    <div className={largeBox}>わかりやすいように色つけてる！
+        <Box className={logo} >
+          <img className={img} src="https://bit.ly/3teQMUw" alt="GirNovel"></img>
+        </Box>
+        <Box className={search}>
+          <Stack spacing={3}>
+            <Input className={searchForm}
+              focusBorderColor='#FD91AB'
+              placeholder='検索'
+            />
+          </Stack>
+        </Box>
+      
     </div> 
   )
 }
